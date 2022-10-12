01acc3dcdadbfbfcd9fff77dbd1ece794476401c
amldimsed <ads,dark@gmail.com>
2022-10-12 10:33:18 -0400

    Delete pregunta1 directory

--- pregunta1/index.php
+++
@@ -1,2 +0,0 @@
-<?php include "miestilo-inicio.ads.php"; ?>
-<?php include "miestilo-final.ads.php"; ?>
--- pregunta1/materia/fis-final.ads.php
+++
@@ -1,6 +0,0 @@
-                </p>
-                
-            </div>
-          </div>
-    </body>
-</html>
